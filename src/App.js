a2ac03c9ff0d228b2edd6f045968907a486ee83f
Your Name <darwinmuthu450@gmail.com>
2020-11-29 06:13:07 +0530

    data is fetched and table created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,48 @@
 import "./App.css";
 import React, { Component } from "react";
 import axios from "axios";
-export default class App extends Component {}
+export default class App extends Component {
+  state = {
+    users: [],
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      this.setState({
+        users: res.data,
+      });
+    });
+  }
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <h1>User Details</h1>
+        <input id="search-box" placeholder="Search by name..." />
+        <table id="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>City</th>
+              <th>Pincode</th>
+            </tr>
+            {users.map((user) => {
+              return (
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address.street}</td>
+                  <td>{user.address.city}</td>
+                  <td>{user.address.zipcode}</td>
+                </tr>
+              );
+            })}
+          </thead>
+        </table>
+      </div>
+    );
+  }
+}
